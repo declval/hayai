1aa6926761b492e1c15299debcb7228960fa8b5a
declval <partialordering@gmail.com>
2023-03-16 19:58:30 +0700

    Moved text handling to the new class Text

--- main.js
+++ main.js
@@ -1,147 +1,45 @@
+import { Text } from "./text.js";
 import { draggable } from "./draggable.js";
-
-const CHUNK_SIZE = 32;
-
-let textToDisplay = "Touch typing (also called blind typing, or touch keyboarding) is a style of typing.";
-
-let chunks = chunked(textToDisplay, CHUNK_SIZE);
 
 document.addEventListener("DOMContentLoaded", main);
 
-function* chunked(string, length) {
-    let start = 0;
-    let end = length;
-
-    while (end <= string.length) {
-        yield string.substring(start, end);
-        start += length;
-        end += length;
-    }
-
-    if (start < string.length) {
-        yield string.substring(start, string.length);
-    }
-}
-
-function keydown(event) {
-    const settings = document.getElementsByClassName("settings")[0];
-
-    if (settings.classList.contains("settings-show")) {
-        return;
-    }
-
-    const caps = document.getElementsByClassName("keyboard-key-caps-indicator")[0];
-
-    if (event.getModifierState("CapsLock")) {
-        caps.classList.add("keyboard-key-caps-indicator-active");
-    } else {
-        caps.classList.remove("keyboard-key-caps-indicator-active");
-    }
-
-    const keys = document.getElementsByClassName("keyboard-key");
-
-    for (const key of keys) {
-        if (key.dataset.value === event.key || key.dataset.valueAlt === event.key) {
-            key.classList.add("keyboard-key-keydown");
-
-            setTimeout(function () {
-                key.classList.remove("keyboard-key-keydown");
-            }, 100);
-        }
-    }
-
-    // Don't move the cursor if event.key is not a single character
-    if (event.key.length != 1) {
-        return;
-    }
-
-    textCursorMove(event.key);
-}
-
-function textCursorMove(key) {
-    const characters = document.getElementsByClassName("text-character");
-
-    for (const character of characters) {
-        if (character.classList.contains("text-character-cursor")) {
-            if (character.textContent === key) {
-                character.classList.add("text-character-correct");
-            } else {
-                character.classList.add("text-character-incorrect");
-
-                const keys = document.getElementsByClassName("keyboard-key");
-
-                for (const key of keys) {
-                    if (key.dataset.value === character.textContent || key.dataset.valueAlt === character.textContent) {
-                        key.classList.add("keyboard-key-highlight");
-
-                        setTimeout(function () {
-                            key.classList.remove("keyboard-key-highlight");
-                        }, 300);
-                    }
-                }
-            }
-            character.classList.remove("text-character-cursor");
-            try {
-                character.nextSibling.classList.add("text-character-cursor");
-            } catch (e) {
-                const chunk = chunks.next().value;
-
-                if (chunk) {
-                    textInit(chunk);
-                } else {
-                    const text = document.getElementsByClassName("text")[0];
-
-                    text.classList.add("text-rotate");
-
-                    setTimeout(function () {
-                        text.classList.remove("text-rotate");
-
-                        chunks = chunked(textToDisplay, CHUNK_SIZE);
-                        textInit(chunks.next().value);
-                    }, 800);
-                }
-            }
-            break;
-        }
-    }
-}
-
-function textInit(string) {
-    string = string.replace(/[^ -~]+/g, "");
-
-    textRemove();
-
-    const text = document.getElementsByClassName("text")[0];
-
-    for (const character of string) {
-        const div = document.createElement("div");
-
-        div.classList.add("text-character");
-
-        if (character === " ") {
-            div.classList.add("text-character-space");
-        }
-
-        div.appendChild(document.createTextNode(character));
-
-        text.appendChild(div);
-    }
-
-    text.firstChild.classList.add("text-character-cursor");
-}
-
-function textRemove() {
-    const text = document.getElementsByClassName("text")[0];
-
-    while (text.firstChild) {
-        text.firstChild.remove();
-    }
-}
-
 function main(event) {
-    textInit(chunks.next().value);
+    let text = new Text();
 
-    document.addEventListener("keydown", keydown);
+    document.addEventListener("keydown", function (event) {
+        const settings = document.getElementsByClassName("settings")[0];
+
+        if (settings.classList.contains("settings-show")) {
+            return;
+        }
+
+        const caps = document.getElementsByClassName("keyboard-key-caps-indicator")[0];
+
+        if (event.getModifierState("CapsLock")) {
+            caps.classList.add("keyboard-key-caps-indicator-active");
+        } else {
+            caps.classList.remove("keyboard-key-caps-indicator-active");
+        }
+
+        const keys = document.getElementsByClassName("keyboard-key");
+
+        for (const key of keys) {
+            if (key.dataset.value === event.key || key.dataset.valueAlt === event.key) {
+                key.classList.add("keyboard-key-keydown");
+
+                setTimeout(function () {
+                    key.classList.remove("keyboard-key-keydown");
+                }, 100);
+            }
+        }
+
+        // Don't move the cursor if event.key is not a single character
+        if (event.key.length != 1) {
+            return;
+        }
+
+        text.cursorMove(event.key);
+    });
 
     const settings = document.getElementsByClassName("settings")[0];
 
@@ -183,10 +81,8 @@ function main(event) {
         event.preventDefault();
 
         if (document.getElementsByClassName("settings-usertext")[0].value.length) {
-            textToDisplay = document.getElementsByClassName("settings-usertext")[0].value;
+            text = new Text(document.getElementsByClassName("settings-usertext")[0].value);
         }
-        chunks = chunked(textToDisplay, CHUNK_SIZE);
-        textInit(chunks.next().value);
 
         settings.classList.remove("settings-show");
     });
---
+++ text.js
@@ -0,0 +1,111 @@
+export { Text };
+
+class Text {
+    constructor(text = Text.initialText, chunkSize = Text.chunkSize) {
+        this.text = text.replace(/[^ -~]+/g, "");
+        this.chunkSize = chunkSize;
+
+        this.characters = null;
+        this.chunks = this.chunked();
+        this.chunk = this.chunks.next().value;
+        this.textElement = document.getElementsByClassName("text")[0];
+
+        this.render();
+    }
+
+    clear() {
+        while (this.textElement.firstChild) {
+            this.textElement.firstChild.remove();
+        }
+    }
+
+    cursorMove(key) {
+        for (const character of this.characters) {
+            if (character.classList.contains("text-character-cursor")) {
+                if (character.textContent === key) {
+                    character.classList.add("text-character-correct");
+                } else {
+                    character.classList.add("text-character-incorrect");
+
+                    const keys = document.getElementsByClassName("keyboard-key");
+
+                    for (const key of keys) {
+                        if (key.dataset.value === character.textContent || key.dataset.valueAlt === character.textContent) {
+                            key.classList.add("keyboard-key-highlight");
+
+                            setTimeout(function () {
+                                key.classList.remove("keyboard-key-highlight");
+                            }, 300);
+
+                            break;
+                        }
+                    }
+                }
+
+                character.classList.remove("text-character-cursor");
+
+                try {
+                    character.nextSibling.classList.add("text-character-cursor");
+                } catch (e) {
+                    this.chunk = this.chunks.next().value;
+
+                    if (this.chunk) {
+                        this.render();
+                    } else {
+                        this.chunks = this.chunked();
+                        this.chunk = this.chunks.next().value;
+
+                        this.textElement.classList.add("text-rotate");
+
+                        const that = this;
+                        setTimeout(function () {
+                            that.textElement.classList.remove("text-rotate");
+                            that.render();
+                        }, 800);
+                    }
+                }
+
+                break;
+            }
+        }
+    }
+
+    render() {
+        this.clear();
+
+        for (const character of this.chunk) {
+            const div = document.createElement("div");
+
+            div.classList.add("text-character");
+
+            if (character === " ") {
+                div.classList.add("text-character-space");
+            }
+
+            div.appendChild(document.createTextNode(character));
+
+            this.textElement.appendChild(div);
+        }
+
+        this.characters = document.getElementsByClassName("text-character");
+        this.textElement.firstChild.classList.add("text-character-cursor");
+    }
+
+    *chunked() {
+        let start = 0;
+        let end = this.chunkSize;
+
+        while (end <= this.text.length) {
+            yield this.text.substring(start, end);
+            start += this.chunkSize;
+            end += this.chunkSize;
+        }
+
+        if (start < this.text.length) {
+            yield this.text.substring(start, this.text.length);
+        }
+    }
+
+    static chunkSize = 32;
+    static initialText = "Touch typing (also called blind typing, or touch keyboarding) is a style of typing.";
+}
